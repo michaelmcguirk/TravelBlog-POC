ad5a91144b3600b490b6c250650fa72ff95b32c8
Michael McGuirk <mmcguirk@Michaels-MacBook-Pro.local>
2019-04-12 10:44:54 -0500

    added lightbox gallery

--- src/BlogContainer.js
+++ src/BlogContainer.js
@@ -1,17 +1,53 @@
 import React, {Component} from 'react'
 import ContentComponent from './ContentComponent';
 import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
 
 class BlogContainer extends Component {
     constructor(props){
         super(props);
+        this.state = { currentImage: 0 };
+        this.closeLightbox = this.closeLightbox.bind(this);
+        this.openLightbox = this.openLightbox.bind(this);
+        this.gotoNext = this.gotoNext.bind(this);
+        this.gotoPrevious = this.gotoPrevious.bind(this);
+    }
+
+    openLightbox(event, obj) {
+        this.setState({
+            currentImage: obj.index,
+            lightboxIsOpen: true,
+        });
+    }
+    closeLightbox() {
+        this.setState({
+            currentImage: 0,
+            lightboxIsOpen: false,
+        });
+    }
+    gotoPrevious() {
+        this.setState({
+            currentImage: this.state.currentImage - 1,
+        });
+    }
+    gotoNext() {
+        this.setState({
+            currentImage: this.state.currentImage + 1,
+        });
     }
 
     render(){
         return(
             <div>
                 <ContentComponent place={this.props.place}/>
-                <Gallery photos={this.props.place.gallery}/>
+                <Gallery photos={this.props.place.gallery} onClick={this.openLightbox}/>
+                <Lightbox images={this.props.place.gallery}
+                    onClose={this.closeLightbox}
+                    onClickPrev={this.gotoPrevious}
+                    onClickNext={this.gotoNext}
+                    currentImage={this.state.currentImage}
+                    isOpen={this.state.lightboxIsOpen}
+                />
             </div> 
         );
     }
