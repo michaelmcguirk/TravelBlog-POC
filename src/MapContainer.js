7e58f6814ddc5441d9e151e9482bb5bdc017b7d1
Michael McGuirk <mmcguirk@Michaels-MacBook-Pro.local>
2019-05-07 18:23:34 -0500

    tidied up some commented out code"

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -3,26 +3,6 @@ import { withGoogleMap, GoogleMap, withScriptjs, Marker} from "react-google-maps
 import mapStyles from './MapStyles';
 import { withRouter } from 'react-router-dom';
 
-/* const MapContainer = withScriptjs(withGoogleMap((props) => 
-    <GoogleMap 
-        defaultZoom={3} 
-        defaultCenter={{lat:-13.387047,lng:-58.460623}}
-        options={{styles: mapStyles, gestureHandling: 'none'}}>
-        {props.entries && props.entries.map((entry, i) => {
-            let lat = parseFloat(entry.location.lat, 10);
-            let lng = parseFloat(entry.location.lng, 10);
-            return(
-                <Marker
-                    key={entry.id}
-                    name={entry.location.address}
-                    position={{ lat: lat, lng: lng }}
-                    onClick={props.setPlace.bind(this, entry)}>
-                </Marker>
-            )
-        })}
-    </GoogleMap>
-)) */
-
 class MapContainer extends Component{
 
     checkPath(){
@@ -53,6 +33,7 @@ class MapContainer extends Component{
                             name={entry.location.address}
                             position={{ lat: lat, lng: lng }}
                             onClick={() => {this.props.setPlace.call(this, entry); this.checkPath.call(this)}}>
+
                         </Marker>
                     )
                 })}
