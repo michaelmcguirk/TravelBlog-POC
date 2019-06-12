de35d1b6c21041a76256778234abee42cd01dc6b
Michael McGuirk <mmcguirk@Michaels-MacBook-Pro.local>
2019-06-12 15:46:26 -0600

    new title and css fixes

--- src/Title.js
+++ src/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = props => {
     return (
-        <div className="title">Six Months</div>
+        <div className="title">Feeding Mosquitos</div>
     )
 }
 
